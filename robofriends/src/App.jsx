321b899a7e97723f14440e7d36c568d91e734062
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-11 22:22:48 +0330

    update

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-class App extends React.Component {
+import { Component } from "react";
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello</h1>
+        <h1>
+         Hello
+        </h1>
       </div>
     );
   }
