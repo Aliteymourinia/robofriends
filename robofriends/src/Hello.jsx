386f7fb445fce819a1091635f7c821ce2fe9f381
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-14 20:06:34 +0330

    update

--- robofriends/src/Hello.jsx
+++ robofriends/src/Hello.jsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from "react";
-
+import "./Hello.css";
 class Hello extends Component {
   render() {
-    return <h1>hello world</h1>;
+    return (
+      <div>
+        <h1>hello world</h1>
+        <p>welcome to react</p>
+      </div>
+    );
   }
 }
 export default Hello;
