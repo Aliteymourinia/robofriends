90560bb326aad451d998dcc1ef6f240eaa1600f4
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-14 21:35:51 +0330

    changed Ali components name to functions for maked it easier syntax

--- robofriends/src/Ali.jsx
+++ robofriends/src/Ali.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-class Ali extends Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.text}</h1>
-      </div>
-    );
-  }
-}
+const Ali = (props) => {
+  return (
+    <div>
+      <h1>{props.text}</h1>
+    </div>
+  );
+};
 export default Ali;
