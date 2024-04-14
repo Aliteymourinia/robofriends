f8d4e6471a287aadfce4116e4b4f58cc3687f4ad
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-14 21:27:26 +0330

    update

--- robofriends/src/Hello.jsx
+++ robofriends/src/Hello.jsx
@@ -1,11 +1,10 @@
-
 import React, { Component } from "react";
 import "./Hello.css";
 class Hello extends Component {
   render() {
     return (
-      <div className="">
-        <h1 className="text-purple-500">hello world</h1>
+      <div>
+        <h1 className="font-bold text-5xl text-purple-500">hello world</h1>
         <p>{this.props.greeting}</p>
       </div>
     );
