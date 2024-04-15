35b9243408989a0a98c4bab546444e4df0c4d8a5
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-15 21:41:14 +0330

    created card component

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
 const Card = () => {
   return (
-    <div>
-      <img src="" />
+    <div className="w-52 bg-green-300 text-black rounded-lg py-6 m-3 ">
+      <img alt="robot" src="https://robohash.org/test" />
       <div>
-        <h2>Jane Doe </h2>
-        <p>jane.doe@gmail.com</p>
+        <h2 className="font-bold text-lg">Jane Doe </h2>
+        <p className="text-sm">jane.doe@gmail.com</p>
       </div>
     </div>
   );
 };
+export default Card;
--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "tachyons";
 import Card from "./Card";
+import "tachyons";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
+    <Card />
+    <Card />
     <Card />
   </div>
 );
