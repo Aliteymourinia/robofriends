860602acbf7b358f852486b76bce01d1a4fa1c52
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-15 20:35:29 +0330

    update

--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "tachyons";
-
+import Card from "./Card";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
-
+    <Card />
   </div>
 );
