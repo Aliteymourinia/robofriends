415b57660ca55935c4aba270287d2a339fd5e224
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-15 22:35:58 +0330

    update

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 const Card = () => {
   return (
-    <div className="w-52 bg-green-300 text-black rounded-lg py-6 m-3 ">
+    <div className="bg-green-300 text-black rounded-lg py-6 m-3">
       <img alt="robot" src="https://robohash.org/test" />
       <div>
-        <h2 className="font-bold text-lg">Jane Doe </h2>
-        <p className="text-sm">jane.doe@gmail.com</p>
+        <h2>Jane Doe </h2>
+        <p>jane.doe@gmail.com</p>
       </div>
     </div>
   );
