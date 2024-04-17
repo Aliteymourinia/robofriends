a982cb5ee5b31701d100b32ef95c153ffec7e2c4
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-17 13:27:51 +0330

    update

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./index.css";
-const Card = (props) => {
+const Card = ( props ) => {
   return (
     <div className="text-black p-4">
       <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
         <img alt="robot" src={props.src} />
-        <h2>{} </h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{props.name} </h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"
+import "./index.css";
 import Card from "./Card";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="card-container">
-    <Card src="https://robohash.org/1"/>
-    <Card src="https://robohash.org/2" />
-    <Card src="https://robohash.org/9"/>
-    <Card src="https://robohash.org/8"/>
-    <Card src="https://robohash.org/5"/>
-    <Card src="https://robohash.org/6"/>
+    <Card name="Jane Doe" email="jane.doe@gmail.com" src="https://robohash.org/1" />
+    <Card name="ali" email="ali.dev@gmail.com" src="https://robohash.org/2" />
+    <Card src="https://robohash.org/9" />
+    <Card src="https://robohash.org/8" />
+    <Card src="https://robohash.org/5" />
+    <Card src="https://robohash.org/6" />
   </div>
 );
