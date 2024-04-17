3f24bafb84b6241d35389b4bf2db39c113b4af67
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-17 16:01:28 +0330

    add note for props destructuring

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
-const Card = (props) => {
-  const { name, email, id } = props; //destructuring of props (used to extract specific values from the props object and it makes the code more readable and clean. )
+const Card = ({ name, email, id }) => {
+  //destructuring of props (used to extract specific values from the props object and it makes the code more readable and clean. )
   return (
     <div className="text-black py-4">
       <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
@@ -13,3 +13,17 @@ const Card = (props) => {
   );
 };
 export default Card;
+
+//note: we can merge the props object to card object to makes the code cleaner like this:
+/*const Card = ({name, email, id}) => {
+  return (
+    <div className="text-black py-4">
+      <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
+        <img alt="robot" src={`https://robohash.org/${id}`} />
+        <h2>{name} </h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+export default Card;*/
