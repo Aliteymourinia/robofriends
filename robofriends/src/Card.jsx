0d0f843dfef0b16e349f243769c089aebd9998ac
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-17 13:02:42 +0330

    added scale and transition for hovering

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./index.css";
 const Card = () => {
   return (
-    <div className="text-black p-3">
-      <div className=" bg-green-300 rounded-lg grow">
+    <div className="text-black p-4">
+      <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
         <img alt="robot" src="https://robohash.org/test" />
         <h2>Jane Doe </h2>
         <p>jane.doe@gmail.com</p>
