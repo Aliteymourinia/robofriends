04666d385eacff76b99a5486d7fc3e2f579abba8
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-17 13:11:00 +0330

    added props to component

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./index.css";
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="text-black p-4">
       <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
-        <img alt="robot" src="https://robohash.org/test" />
-        <h2>Jane Doe </h2>
+        <img alt="robot" src={props.src} />
+        <h2>{} </h2>
         <p>jane.doe@gmail.com</p>
       </div>
     </div>
--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -5,10 +5,11 @@ import Card from "./Card";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="card-container">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card src="https://robohash.org/1"/>
+    <Card src="https://robohash.org/2" />
+    <Card src="https://robohash.org/9"/>
+    <Card src="https://robohash.org/8"/>
+    <Card src="https://robohash.org/5"/>
+    <Card src="https://robohash.org/6"/>
   </div>
 );
