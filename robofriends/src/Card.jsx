4cd46a24a4962cb89e3e0f3c4e80934a9d22cec2
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-17 15:26:21 +0330

    up

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./index.css";
-import { robots } from "./robots";
 const Card = (props) => {
   return (
     <div className="text-black py-4">
       <div className=" bg-green-300 rounded-lg  hover:scale-110 transition-transform duration-200">
-        <img alt="robot" src={props.src} />
+        <img alt="robot" src="https://robohash.org/5" />
         <h2>{props.name} </h2>
         <p>{props.email}</p>
       </div>
--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -5,41 +5,11 @@ import Card from "./Card";
 import { robots } from "./robots";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="card-container">
-    <Card
-      id={robots[0].id}
-      name={robots[0].name}
-      email={robots[0].email}
-      src="https://robohash.org/1"
-    />
-    <Card
-      id={robots[1].id}
-      name={robots[1].name}
-      email={robots[1].email}
-      src="https://robohash.org/2"
-    />
-    <Card
-      id={robots[2].id}
-      name={robots[2].name}
-      email={robots[2].email}
-      src="https://robohash.org/8"
-    />
-    <Card
-      id={robots[3].id}
-      name={robots[3].name}
-      email={robots[3].email}
-      src="https://robohash.org/5"
-    />
-    <Card
-      id={robots[4].id}
-      name={robots[4].name}
-      email={robots[4].email}
-      src="https://robohash.org/14"
-    />
-    <Card
-      id={robots[5].id}
-      name={robots[5].name}
-      email={robots[5].email}
-      src="https://robohash.org/9"
-    />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
   </div>
 );
