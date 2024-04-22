c14813e413245e0afbbf5154572d526664f6ae3d
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-22 20:45:08 +0330

    create App component

--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 import CardList from "./CardList";
 import { robots } from "./robots";
 ReactDOM.createRoot(document.getElementById("root")).render(
