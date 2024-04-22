dc398ea0e0cb55bde4ab3672e4bc6867e556edc5
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-22 21:30:29 +0330

    up

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-
+import CardList from "./CardList";
+import { robots } from "./robots";
 const App = () => {
-    return(
-        <CardList robots={robots} />
-        
-    );
+  return <CardList robots={robots} />;
 };
--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import CardList from "./CardList";
-import { robots } from "./robots";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
  
 );
