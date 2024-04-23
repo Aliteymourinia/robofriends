516710773b6a07ae7e13ac1c04ac7fc3b4016716
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-23 16:47:34 +0330

    up

--- robofriends/src/SearchBox.jsx
+++ robofriends/src/SearchBox.jsx
@@ -5,8 +5,8 @@ const SearchBox = () => {
     <div className="p-2 text-center">
       <input
         type="search"
-        placeholder="search robots"
-        className="p-3 text-black bg-blue-300 placeholder-gray-500  box-border"
+        placeholder="Search robots"
+        className="p-3 text-black bg-blue-300 placeholder-gray-500  box-border border-2 border-green-300 focus:outline-none focus:shadow-lg shadow-blue-300 focus:border-blue-500 "
       />
     </div>
   );
