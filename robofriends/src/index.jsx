965df206333fae3b42d71c84fab411f3fe0f5ada
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-23 11:26:27 +0330

    update

--- robofriends/src/index.jsx
+++ robofriends/src/index.jsx
@@ -2,6 +2,5 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import CardList from "./CardList";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
