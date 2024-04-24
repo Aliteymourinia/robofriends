c68b4047cbcb5c19f30476ba711ca447e9f94a3b
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-24 12:26:32 +0330

    update

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
-const App = () => {
-  return (
-    <div className="text-center">
-      <h1>RoboFriends</h1>
-      <SearchBox/>
-      <CardList robots={robots}/>;
-    </div>
-  );
+
+const state = {
+  robots: robots,
+  searchfield: "",
 };
+
+class App extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
 export default App;
--- robofriends/src/CardList.jsx
+++ robofriends/src/CardList.jsx
@@ -12,6 +12,6 @@ const CardList = ({ robots }) => {
     );
   });
 
-  return <div className="card-container">{CardArray}</div>;
+  return <div className="card-container ">{CardArray}</div>;
 };
 export default CardList;
