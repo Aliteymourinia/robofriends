fc9b21537b860773b7ce129d2de3638676086f92
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-25 16:09:08 +0330

    add scroll for cardlist

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 class App extends Component {
   constructor() {
     super();
