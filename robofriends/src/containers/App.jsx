99efdf8b3019bec3377025efe8afd92dfb9f6250
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-26 19:43:43 +0330

    moved components tp folder

--- robofriends/src/containers/App.jsx
+++ robofriends/src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
-import Scroll from "./components/Scroll";
 class App extends Component {
   constructor() {
     super();
