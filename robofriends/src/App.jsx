bbda1705f0ff7b59507b133d047eb902b01e88ad
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-04-26 19:39:55 +0330

    moved components

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./components/CardList";
-import SearchBox from "./SearchBox";
+import SearchBox from "./components/SearchBox";
 import "./App.css";
 import Scroll from "./components/Scroll";
 class App extends Component {
