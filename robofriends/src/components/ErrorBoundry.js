bb29c1f0e7439f74e59e82294a0245f3af4b0e53
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-05-01 10:40:02 +0330

    update

--- robofriends/src/components/ErrorBoundry.js
+++ robofriends/src/components/ErrorBoundry.js
@@ -1,1 +1,16 @@
 import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h1>that is not good!</h1>;
+    }
+  }
+}
+export default ErrorBoundary;
