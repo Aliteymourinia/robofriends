0aba4e022ddcbb54deb125c25e834f40ae7cfa32
Aliteymourinia <aliteymourinia.dev@gmail.com>
2024-05-06 19:50:22 +0330

    update

--- robofriends/vite.config.js
+++ robofriends/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // vite.config.js
 export default {
-  base: "/robofriends/",
+  base: "/my-react-app/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
